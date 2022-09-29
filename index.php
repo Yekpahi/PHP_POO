78e6651f8cd3bc8731c3bb88a0047c5df73063d0
Yekpahi Prince <princeyekpahi@hotmail.fr>
2022-09-29 11:25:02 +0200

    const et static

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
+<?php
+declare(strict_types=1);
 
+class Pont 
+{
+    public static int $nombrepietons = 0;
+
+    public function nouveauPieton()
+    {
+        self::$nombrepietons++;
+    }
+}
+
+$pontLondre = new Pont();
+$pontLondre->nouveauPieton();
+
+$pontParis = new Pont();
+$pontParis->nouveauPieton();
+
+$pontZou = new Pont();
+$pontZou->nouveauPieton();
+
+echo "Nombre de pont : " .Pont::$nombrepietons;
